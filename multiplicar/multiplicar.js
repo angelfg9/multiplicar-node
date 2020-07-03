755420c969c638d33d02c1c4074931758970ccd6
Angel <angelguerrero425@gmail.com>
2020-07-03 02:13:51 -0600

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,47 @@
+/**
+ *  Requires
+ */
+const fs = require('fs');
+
+let listarTabla = (base, limite) => {
+
+    console.log('===================='.green);
+    console.log(`===  tabla de ${base}  ===`.green);
+    console.log('===================='.green);
+
+    for (let i = 1; i <= limite; i++) {
+
+        console.log(`${base}*${i} = ${base*i}`);
+
+    }
+
+}
+
+let crearArchivo = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`EL valor introducido "${base}" no es un numero`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${base}*${i} = ${base*i}\n`;
+        }
+
+
+        fs.writeFile(`tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
+            if (err) reject(err);
+            else
+                resolve(`tabla-${base}-al-${limite}.txt`);
+        });
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
